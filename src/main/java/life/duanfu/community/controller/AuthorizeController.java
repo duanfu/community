1f20b9bcbbf2bb28f3a892308af7725d7cf97f0f
断浮 <1658439160@qq.com>
2020-02-23 21:31:09 +0800

    insert H2

--- src/main/java/life/duanfu/community/controller/AuthorizeController.java
+++ src/main/java/life/duanfu/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package life.duanfu.community.controller;
 
 import life.duanfu.community.dto.AccessTokenDTO;
 import life.duanfu.community.dto.GithubUser;
+import life.duanfu.community.mapper.UserMapper;
+import life.duanfu.community.model.User;
 import life.duanfu.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -25,6 +28,9 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
@@ -36,10 +42,17 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        if (user != null) {
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null) {
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
             //登录成功，写cookie和session
-            request.getSession().setAttribute("user",user);
+            request.getSession().setAttribute("user", githubUser);
             //重定向到index页面
             return "redirect:/";
         } else {
