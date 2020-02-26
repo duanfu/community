a681594b01c97577f0745253d62d280c0fc4096e
断浮 <1658439160@qq.com>
2020-02-27 00:39:12 +0800

    add limit

--- src/main/java/life/duanfu/community/controller/IndexController.java
+++ src/main/java/life/duanfu/community/controller/IndexController.java
@@ -1,6 +1,6 @@
 package life.duanfu.community.controller;
 
-import life.duanfu.community.dto.QuestionDTO;
+import life.duanfu.community.dto.PaginationDTO;
 import life.duanfu.community.mapper.UserMapper;
 import life.duanfu.community.model.User;
 import life.duanfu.community.service.QuestionService;
@@ -8,10 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
 public class IndexController {
@@ -24,21 +24,24 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request,
-                        Model model){
+                        Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "5") Integer size) {
         Cookie[] cookies = request.getCookies();
         for (Cookie cookie : cookies) {
-            if(cookie != null && cookies.length !=0)
-            if (cookie.getName().equals("token")){
-                String token = cookie.getValue();
-                User user = userMapper.findByToken(token);
-                if (user!=null){
-                    request.getSession().setAttribute("user",user);
+            if (cookie != null && cookies.length != 0)
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
                 }
-                break;
-            }
         }
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions",questionList);
+
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination", pagination);
         return "index";
     }
 
--- src/main/java/life/duanfu/community/service/QuestionService.java
+++ src/main/java/life/duanfu/community/service/QuestionService.java
@@ -1,5 +1,6 @@
 package life.duanfu.community.service;
 
+import life.duanfu.community.dto.PaginationDTO;
 import life.duanfu.community.dto.QuestionDTO;
 import life.duanfu.community.mapper.QuestionMapper;
 import life.duanfu.community.mapper.UserMapper;
@@ -19,9 +20,24 @@ public class QuestionService {
     @Autowired
     private UserMapper userMapper;
 
-    public List<QuestionDTO> list() {
-        List<Question> questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size) {
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount,page,size);
+        //保证page>totalPage，的时候数据会显示到totalPage这页，如果删掉数据不展示
+        //在DTO判断只能让数字切换回来数据不显示，必须在数据库层面，判断一次。为了不复写代码，加了一个字段totalPage
+        if (page < 1) {
+            page = 1;
+        }
+        if (page > paginationDTO.getTotalPage()) {
+            page = paginationDTO.getTotalPage();
+        }
+        //size*(page-1)
+        Integer offset = size *(page-1);
+        List<Question> questions = questionMapper.list(offset,size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
+
+
         for (Question question : questions) {
             User user = userMapper.findById(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
@@ -30,6 +46,7 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+        return paginationDTO;
     }
 }
