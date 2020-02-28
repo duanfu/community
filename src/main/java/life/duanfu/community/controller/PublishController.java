433ae04e4a1e4caf9a71f4e923b3d51b5c7dafcb
断浮 <1658439160@qq.com>
2020-02-28 15:27:12 +0800

    add interceptor

--- src/main/java/life/duanfu/community/controller/PublishController.java
+++ src/main/java/life/duanfu/community/controller/PublishController.java
@@ -1,7 +1,6 @@
 package life.duanfu.community.controller;
 
 import life.duanfu.community.mapper.QuestionMapper;
-import life.duanfu.community.mapper.UserMapper;
 import life.duanfu.community.model.Question;
 import life.duanfu.community.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
@@ -19,9 +17,6 @@ public class PublishController {
 
     @Autowired
     private QuestionMapper questionMapper;
-
-    @Autowired
-    private UserMapper userMapper;
 
     @GetMapping("/publish")
     public String publish() {
@@ -60,20 +55,7 @@ public class PublishController {
         }
 
         //获取User
-        User user = null;
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0)
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-
+        User user = (User) request.getSession().getAttribute("user");
         if (user == null) {
             model.addAttribute("error", "用户未登录");
             return "publish";
---
+++ src/main/java/life/duanfu/community/interceptor/WebConfig.java
@@ -0,0 +1,20 @@
+package life.duanfu.community.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private SessionInterceptor sessionInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //请求的时候，addPathPatterns：会拦截的地址
+        //excludePathPatterns：略过的地址
+        //我们希望所有的路径都检验一下
+        registry.addInterceptor(sessionInterceptor).addPathPatterns("/**");
+    }
+}
